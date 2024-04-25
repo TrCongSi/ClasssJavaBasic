c7522ee37abca2f985b4b44c822a27e2fea663da
Tr.CongSi <shimano12311@gmail.com>
2024-04-25 15:44:39 +0700

    Buổi 1

---
+++ Demo/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        System.out.println("Em chào anh Leon, em là Trịnh Công Sỉ  sinh năm 2001");
+        System.out.println("Học CNTT, trường Đại học Công Thương");
+    }
+}
